5a67d93658d8a523f3d260320142c3f27123cc72
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-05 11:04:03 +0200

    respuesta OK y H2

---
+++ colegio/src/main/java/com/colegio/controller/StatusController.java
@@ -0,0 +1,13 @@
+package com.colegio.controller; 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class StatusController{
+    @GetMapping("/status")
+public String status() {
+    return "OK";
+}
+}
