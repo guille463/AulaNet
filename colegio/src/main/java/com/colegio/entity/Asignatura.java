bb85934c395c0120bdc7b4d770b9b7f7f4103742
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-05 17:04:58 +0200

    feat: entidad Asignatura

---
+++ colegio/src/main/java/com/colegio/entity/Asignatura.java
@@ -0,0 +1,91 @@
+package com.colegio.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="asignaturas")
+public class Asignatura {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id; 
+    private String codigo;
+    private String nombre; 
+    private int creditos; 
+    private String curso; 
+    private int horasSemana;  
+
+    public Asignatura() {
+    }
+    
+    public Asignatura(String codigo, int creditos, String curso, int horasSemana, String nombre) {
+        this.codigo = codigo;
+        this.creditos = creditos;
+        this.curso = curso;
+        this.horasSemana = horasSemana;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getHorasSemana() {
+        return horasSemana;
+    }
+
+    public void setHorasSemana(int horasSemana) {
+        this.horasSemana = horasSemana;
+    }
+
+    
+     @Override
+  
+      public String toString() {
+    return "Alumno {id = " + id + ", nombre = " + nombre + ", curso = " + curso + ", Creditos: " + creditos + ", Curso: " + curso + ",HorasSemana: " + horasSemana  + "}";
+      
+}
+
+
+    
+}
+
+
--- colegio/src/main/java/com/colegio/util/Constantes.java
+++ colegio/src/main/java/com/colegio/util/Constantes.java
@@ -4,5 +4,6 @@ public class Constantes {
     //Prefijos para los codigos de identificacion
     public static final String PREFIJO_ALUMNO = "ALUM-";
     public static final String PREFIJO_PROFESOR = "PROF-";
+    public static final String PREFIJO_ASIG = "ASG-"; 
 
 }
