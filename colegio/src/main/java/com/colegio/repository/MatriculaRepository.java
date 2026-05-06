3ff0ff87bbcd310bdb5fca9e2170ce05c6aa87c0
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-06 11:18:42 +0200

    feat: MatriculaRepository

---
+++ colegio/src/main/java/com/colegio/repository/MatriculaRepository.java
@@ -0,0 +1,12 @@
+package com.colegio.repository; 
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.colegio.entity.Matricula;
+
+@Repository
+public interface MatriculaRepository extends JpaRepository<Matricula, Long>{
+    
+}
+
