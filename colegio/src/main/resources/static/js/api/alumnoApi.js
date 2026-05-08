8aab53192918fdf2e6a9dd642f3708693a6806ef
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-08 12:38:43 +0200

    fix: error en la escritura de la funcion en alumnosApi

---
+++ colegio/src/main/resources/static/js/api/alumnoApi.js
@@ -0,0 +1,9 @@
+import {BASE_URL} from "../config/config.js"; 
+
+const URL_ALUMNOS = BASE_URL + "/alumnos"; 
+
+export async function getAlumnos() {
+    const respuesta = await fetch(URL_ALUMNOS); 
+    return await respuesta.json(); 
+    
+}
