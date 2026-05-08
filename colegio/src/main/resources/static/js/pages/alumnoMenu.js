20e53d6fec661ed227462f2ce3865f93a62cde23
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-08 12:51:36 +0200

    feat: alumnoMenu js y html

---
+++ colegio/src/main/resources/static/js/pages/alumnoMenu.js
@@ -0,0 +1,13 @@
+import {crearBarraNavegacion} from "../components/navbar.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("navbar").innerHTML = crearBarraNavegacion();
+
+    document.getElementById("root").innerHTML = `
+        <div class="containerMenuAlumnos">
+            <h1 class="MenuAlumnos">Gestion de Alumnos</h1>
+                <a href="alumnoLista.html" class="btn btn-Lista">Ver todos los alumnos</a>
+            </div>
+        </div>
+    `;
+});
