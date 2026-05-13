e392471e1fa53b6de1bda21ee7937bb774e3f13d
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-13 12:35:54 +0200

    feat: funcion util para refactorizar codigo

---
+++ colegio/src/main/resources/static/js/utils/apiUtils.js
@@ -0,0 +1,30 @@
+import { API_CONFIG } from "../config/apiConfig.js";
+
+export async function fetchApi(metodo, ruta, cuerpo = null) {
+    let datos = null;
+    let error = null;
+    try {
+        const opciones = {};
+        opciones.method = metodo;
+        opciones.headers = { "Content-Type": "application/json" };
+
+        if (cuerpo !== null) {
+            opciones.body = JSON.stringify(cuerpo);
+        }
+
+        const respuesta = await fetch(API_CONFIG.BASE_URL + ruta, opciones);
+
+        if (respuesta.ok) {
+            if (metodo === "DELETE") {
+                datos = true;
+            } else {
+                datos = await respuesta.json();
+            }
+        } else {
+            error = await respuesta.text();
+        }
+    } catch (e) {
+        console.error(e);
+    }
+    return { datos, error };
+}
