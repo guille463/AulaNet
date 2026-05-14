a49b4e51294f9b7a9ca524033d8db14d2dc9cb36
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-14 12:10:14 +0200

    feat: asignaturaApi y componente

---
+++ colegio/src/main/resources/static/js/api/asignaturaApi.js
@@ -0,0 +1,7 @@
+import { fetchApi } from "../utils/apiUtils.js";
+
+export const AsignaturaAPI = {
+    obtenerTodos: () => fetchApi('GET', '/asignaturas'),
+    obtenerPorId: (id) => fetchApi('GET', `/asignaturas/${id}`),
+    obtenerPorCurso: (curso) => fetchApi('GET', `/asignaturas/curso/${curso}`)
+};
---
+++ colegio/src/main/resources/static/js/components/asignaturaComponente.js
@@ -0,0 +1,13 @@
+export function crearTarjetaAsignatura(asignatura) {
+    return `
+        <div class="card">
+            <div class="card-body" style="display:flex; gap:20px;">
+                <strong>${asignatura.codigo}</strong>
+                <span>${asignatura.nombre}</span>
+                <span>${asignatura.curso}</span>
+                <span>${asignatura.horasSemana}h/semana</span>
+                <a href="detalleAsignatura.html?id=${asignatura.id}">👁</a>
+            </div>
+        </div>
+    `;
+}
