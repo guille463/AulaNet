2f41c8aa7970e3aaa8d2ee76a3847094febf9bdf
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-14 10:50:33 +0200

    feat: añlado funciones para asignar tutor al aula y eliminarlo

--- colegio/src/main/resources/static/js/api/aulaApi.js
+++ colegio/src/main/resources/static/js/api/aulaApi.js
@@ -2,7 +2,14 @@ import { fetchApi } from "../utils/apiUtils.js";
 
 export const AulaAPI = {
     obtenerTodos: () => fetchApi('GET', '/aulas'), 
+   
     obtenerPorCodigo: (codigo) => fetchApi('GET', `/aulas/codigo/${encodeURIComponent(codigo)}`),
+    
     obtenerPorId: (id) => fetchApi('GET', `/aulas/${id}`),
-    calcularAlumnos: (id) => fetchApi('GET', `/aulas/${id}/alumnos/count`)
+    
+    calcularAlumnos: (id) => fetchApi('GET', `/aulas/${id}/alumnos/count`),
+    
+    asignarTutor: (aulaId, profesorId) => fetchApi('PUT', `/aulas/${aulaId}/tutor/${profesorId}`),
+
+    eliminarTutor: (aulaId) => fetchApi('DELETE', `/aulas/${aulaId}/tutor`)
 };
