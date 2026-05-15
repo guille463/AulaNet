a41f69b9f8fd0d4ec10705f87c039ee35c18683a
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-15 20:25:57 +0200

    cambio nombre del contenedor

--- colegio/src/main/resources/static/js/pages/alumnoMenu.js
+++ colegio/src/main/resources/static/js/pages/alumnoMenu.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   root.innerHTML = `
-      <div class="containerBuscarAlumno">
+      <div class="containerGestionAlumno">
         <h1>Gestionar Alumnos</h1>
         <a href="crearAlumno.html">➕ Añadir alumno</a>
         <div class="card">
