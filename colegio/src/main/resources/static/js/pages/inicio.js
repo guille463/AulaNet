76b37e924f28d1761e73b7fadfb15fead7f8fa1c
jimenezmunozguillermorafael <jimenezmunozguillermorafael001.alu@stemgranada.com>
2026-05-17 14:28:46 +0200

    fix: elimino los botones debajo del hero de incio.js

--- colegio/src/main/resources/static/js/pages/inicio.js
+++ colegio/src/main/resources/static/js/pages/inicio.js
@@ -1,10 +1,4 @@
 import { crearBarraNavegacion } from "../components/navbar.js";
-document.addEventListener("DOMContentLoaded", async () =>{
-    document.getElementById("navbar").innerHTML = crearBarraNavegacion(); 
-    const root = document.getElementById("root"); 
-    root.innerHTML = `
-        <div class="containerInicio">
-            <h1>Gestion colegio</h1>
-        </div>
-    `;
-})
+document.addEventListener("DOMContentLoaded", async () => {
+    document.getElementById("navbar").innerHTML = crearBarraNavegacion();
+});
